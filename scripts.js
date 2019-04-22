bd1667c51e8035f7d63afa5b7b7b19827f1f8107
Victor Johnson <vicjohnson1213@gmail.com>
2019-04-21 22:59:12 -0500

    Use dark gray theme instead of blue.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,5 @@
 (function() {
-    const clock$ = document.getElementById('clock');
-    const date$ = document.getElementById('date');
     const bookmarks$ = document.getElementById('bookmarks');
-
-    setClock();
-    setInterval(setClock, 1000);
-
-    function setClock() {
-        const now = new Date();
-        date$.innerText = now.toLocaleDateString('en-US', {
-            weekday: 'long',
-            month: 'long',
-            day: 'numeric'
-        });
-
-        clock$.innerText = now.toLocaleTimeString('en-US', {
-            hour12: false,
-            hour: 'numeric',
-            minute: 'numeric'
-        });
-    }
 
     chrome.bookmarks.search('homepage', (results) => {
         if (!results.length) {
@@ -51,6 +31,7 @@
                         const bookmark$ = document.createElement('a');
                         bookmark$.href = bookmark.url;
                         bookmark$.innerText = bookmark.title;
+                        bookmark$.target = '_blank';
                         list$.appendChild(bookmark$);
                     });
                 });
